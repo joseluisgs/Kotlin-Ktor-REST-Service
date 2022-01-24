8d4d2a6cd4075b81524b6b179f3b68941ce7abb9
joseuisgs <joseluisgonsan@gmail.com>
2022-01-24 20:06:13 +0100

    Web Statica

--- src/main/kotlin/es/joseluis/Application.kt
+++ src/main/kotlin/es/joseluis/Application.kt
@@ -1,7 +1,7 @@
 package es.joseluis
 
+import es.joseluis.routes.registerWebContentRouting
 import io.ktor.server.application.*
-import io.ktor.server.http.content.*
 import io.ktor.server.netty.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -9,15 +9,14 @@ import io.ktor.server.routing.*
 fun main(args: Array<String>) = EngineMain.main(args)
 
 fun Application.module() {
+    // Una ruta de presentación
     routing {
         // Entrada en la api
         get("/") {
             call.respondText(" \uD83D\uDC4B Hola Kotlin REST Service con Ktor")
         }
         // Contenido estático, desde la carpeta resoruces cuando entran a /web
-        static("/web") {
-            resources("web")
-            default("index.html")
-        }
     }
+    // Registramos las rutas de la aplicación
+    registerWebContentRouting()
 }
--- src/main/kotlin/es/joseluis/routes/web.kt
+++ src/main/kotlin/es/joseluis/routes/web.kt
@@ -1,4 +1,29 @@
 package es.joseluis.routes
 
-class web {
+import io.ktor.server.application.*
+import io.ktor.server.http.content.*
+import io.ktor.server.routing.*
+
+
+// Ahora creamos una funcion de extensión de Aplication, y exportamos las rutas
+fun Application.registerWebContentRouting() {
+    routing {
+        webContentRoute()
+    }
 }
+
+// Vamos a crear una ruta web, para ello usamos una función de extensión de la clase Router
+// La llamamos webContent y le decimos el contenido que queremos que se muestre
+fun Route.webContentRoute() {
+    // Contenido estático, desde la carpeta resources cuando entran a /web
+    static {
+        // Si nos preguntan por /web desde la raíz, le mandamos el contenido estático. tambien aplicamos redireccion
+        resource("/web", "web/index.html")
+        resource("*", "web/index.html")
+        // todo contenido estático con web/, lo busca en la carpeta web
+        static("web") {
+            resources("web")
+        }
+    }
+}
+

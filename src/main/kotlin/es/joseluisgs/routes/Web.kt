6d0bb2e0931052e177da382bd5e9f7fa39b1b8db
joseuisgs <joseluisgonsan@gmail.com>
2022-01-25 17:15:52 +0100

    Rutas Web

--- src/main/kotlin/es/joseluisgs/routes/Web.kt
+++ src/main/kotlin/es/joseluisgs/routes/Web.kt
@@ -1,20 +1,20 @@
 package es.joseluisgs.routes
 
-import io.ktor.server.application.*
-import io.ktor.server.http.content.*
-import io.ktor.server.routing.*
+import io.ktor.application.*
+import io.ktor.http.content.*
+import io.ktor.routing.*
 
 
 // Ahora creamos una funcion de extensión de Aplication, y exportamos las rutas
-fun Application.registerWebContentRouting() {
+fun Application.webRoutes() {
     routing {
-        webContentRoute()
+        webRoutes()
     }
 }
 
 // Vamos a crear una ruta web, para ello usamos una función de extensión de la clase Router
 // La llamamos webContent y le decimos el contenido que queremos que se muestre
-fun Route.webContentRoute() {
+fun Route.webRoutes() {
     // Contenido estático, desde la carpeta resources cuando entran a /web
     static {
         // Si nos preguntan por /web desde la raíz, le mandamos el contenido estático. tambien aplicamos redireccion

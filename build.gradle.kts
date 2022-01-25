773539c15f2d6fddecad98ade8d5322075f9e437
joseuisgs <joseluisgonsan@gmail.com>
2022-01-25 17:14:14 +0100

    Gradle Ktor 1.6.7

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,22 @@
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
 val ktor_version: String by project
 val kotlin_version: String by project
 val logback_version: String by project
+val serialization_version: String by project
 
 plugins {
     application
     kotlin("jvm") version "1.6.10"
+    // serialización de objetos
+    kotlin("plugin.serialization") version "1.6.10"
     // Si queremos usar shadowJAR: https://ktor.io/docs/fatjar.html#run
     id("com.github.johnrengelman.shadow") version "7.0.0"
 }
 
 group = "es.joseluisgs"
 version = "0.0.1"
+
 application {
     mainClass.set("es.joseluisgs.ApplicationKt")
 }
@@ -29,6 +35,8 @@ dependencies {
     implementation("ch.qos.logback:logback-classic:$logback_version")
     // Poder serializar usando Kotlin serialization
     implementation("io.ktor:ktor-serialization:$ktor_version")
+    // implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:$serialization_version")
+
     // Utilizades para test
     testImplementation("io.ktor:ktor-server-tests:$ktor_version")
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit:$kotlin_version")
@@ -51,5 +59,9 @@ tasks {
         manifest {
             attributes(Pair("Main-Class", "es.joseluisgs.ApplicationKt"))
         }
+    }
+    // Forzamos que se compile el ByteCode para una versión determinada de JVM
+    withType<KotlinCompile> {
+        kotlinOptions.jvmTarget = "11"
     }
 }
